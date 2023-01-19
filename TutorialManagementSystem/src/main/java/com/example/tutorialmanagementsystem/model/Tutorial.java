9f583b011ff9159107c26becc3336a86994c4db1
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-19 23:54:03 +0300

    add tutorial entity

---
+++ TutorialManagementSystem/src/main/java/com/example/tutorialmanagementsystem/model/Tutorial.java
@@ -0,0 +1,35 @@
+package com.example.tutorialmanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tutorials")
+@Data
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tutorial_generator")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+    public Tutorial() {
+
+    }
+
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+}
+

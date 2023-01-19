26da303f8c1bd4d489b08a8d97c59bbfbfaa3416
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-19 23:53:51 +0300

    add comment entity

---
+++ TutorialManagementSystem/src/main/java/com/example/tutorialmanagementsystem/model/Comment.java
@@ -0,0 +1,26 @@
+package com.example.tutorialmanagementsystem.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "comments")
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comment_generator")
+    private Long id;
+
+    @Lob
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tutorial_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Tutorial tutorial;
+
+}

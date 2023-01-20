db090e5a97ed7c57651c9e9c0175567e9d659743
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-20 23:42:36 +0300

    set comment service

--- TutorialManagementSystem/src/main/java/com/example/tutorialmanagementsystem/controller/CommentController.java
+++ TutorialManagementSystem/src/main/java/com/example/tutorialmanagementsystem/controller/CommentController.java
@@ -4,6 +4,7 @@ import com.example.tutorialmanagementsystem.exception.ResourceNotFoundException;
 import com.example.tutorialmanagementsystem.model.Comment;
 import com.example.tutorialmanagementsystem.repository.CommentRepository;
 import com.example.tutorialmanagementsystem.repository.TutorialRepository;
+import com.example.tutorialmanagementsystem.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,61 +21,41 @@ public class CommentController {
     private TutorialRepository tutorialRepository;
 
     @Autowired
+    private CommentService commentService;
+    @Autowired
     private CommentRepository commentRepository;
 
     @GetMapping("/tutorials/{tutorialId}/comments")
     public ResponseEntity<List<Comment>> getAllCommentsByTutorialId(@PathVariable(value = "tutorialId") Long tutorialId) {
-        if (!tutorialRepository.existsById(tutorialId)) {
-            throw new ResourceNotFoundException("Not found Tutorial with id = " + tutorialId);
-        }
-
-        List<Comment> comments = commentRepository.findByTutorialId(tutorialId);
-        return new ResponseEntity<>(comments, HttpStatus.OK);
+        return new ResponseEntity<>( commentService.findByTutorialId(tutorialId), HttpStatus.OK);
     }
 
     @GetMapping("/comments/{id}")
     public ResponseEntity<Comment> getCommentsByTutorialId(@PathVariable(value = "id") Long id) {
-        Comment comment = commentRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Not found Comment with id = " + id));
-
-        return new ResponseEntity<>(comment, HttpStatus.OK);
+        return new ResponseEntity<>(commentService.getCommentByTutorialId(id), HttpStatus.OK);
     }
 
     @PostMapping("/tutorials/{tutorialId}/comments")
     public ResponseEntity<Comment> createComment(@PathVariable(value = "tutorialId") Long tutorialId,
                                                  @RequestBody Comment commentRequest) {
-        Comment comment = tutorialRepository.findById(tutorialId).map(tutorial -> {
-            commentRequest.setTutorial(tutorial);
-            return commentRepository.save(commentRequest);
-        }).orElseThrow(() -> new ResourceNotFoundException("Not found Tutorial with id = " + tutorialId));
-
-        return new ResponseEntity<>(comment, HttpStatus.CREATED);
+        return new ResponseEntity<>(commentService.createComment(tutorialId,commentRequest), HttpStatus.CREATED);
     }
 
     @PutMapping("/comments/{id}")
     public ResponseEntity<Comment> updateComment(@PathVariable("id") long id, @RequestBody Comment commentRequest) {
-        Comment comment = commentRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("CommentId " + id + "not found"));
-
-        comment.setContent(commentRequest.getContent());
-
-        return new ResponseEntity<>(commentRepository.save(comment), HttpStatus.OK);
+        return new ResponseEntity<>(commentService.updateComment(id,commentRequest), HttpStatus.OK);
     }
 
     @DeleteMapping("/comments/{id}")
     public ResponseEntity<HttpStatus> deleteComment(@PathVariable("id") long id) {
-        commentRepository.deleteById(id);
+        commentService.deleteComment(id);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/tutorials/{tutorialId}/comments")
     public ResponseEntity<List<Comment>> deleteAllCommentsOfTutorial(@PathVariable(value = "tutorialId") Long tutorialId) {
-        if (!tutorialRepository.existsById(tutorialId)) {
-            throw new ResourceNotFoundException("Not found Tutorial with id = " + tutorialId);
-        }
-
-        commentRepository.deleteByTutorialId(tutorialId);
+         commentService.deleteAllCommentsOfTutorial(tutorialId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
